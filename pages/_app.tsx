c61e2349e439dc4e083fa49f1212ba2b66313d93
Arturo Quirós <joseq2408@gmail.com>
2022-07-31 10:43:47 -0600

    dark theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { NextUIProvider } from "@nextui-org/react";
+import { darkTheme } from "../themes";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NextUIProvider theme={darkTheme}>
+      <Component {...pageProps} />;
+    </NextUIProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@nextui-org/react";
 import type { NextPage } from "next";
 
 const HomePage: NextPage = () => {
   return (
     <>
       <h1>Hola Mundo</h1>
+      <Button color="gradient">Boton</Button>
     </>
   );
 };
