087adbac79e20d3c0f974c6c0a01a3e377a25be5
Arturo Quirós <joseq2408@gmail.com>
2022-07-31 12:23:55 -0600

    pokeAPI with axios

---
+++ api/index.ts
@@ -0,0 +1,1 @@
+export { default as pokeAPI } from "./pokeAPI";
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import { MainLayout } from "../components/layouts";
 import { GetStaticProps } from "next";
+import pokeAPI from "../api/pokeAPI";
 
 const HomePage: NextPage = (props) => {
   console.log(props);
@@ -21,8 +22,14 @@ const HomePage: NextPage = (props) => {
 
 //server side
 export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { data } = await pokeAPI.get("/pokemon?limit=151");
+
+  console.log(data);
+
   return {
-    props: {},
+    props: {
+      pokemons: data.results,
+    },
   };
 };
 
