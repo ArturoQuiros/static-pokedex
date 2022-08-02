0ebc6583f6e36191fe575266cf5c6f6664c419ec
Arturo Quirós <joseq2408@gmail.com>
2022-08-01 19:34:28 -0600

    pokeCards on favs

--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -2,6 +2,7 @@ import { MainLayout } from "../../components";
 import { NoFavorites } from "../../components/ui";
 import { useState, useEffect } from "react";
 import localFav from "../../utils/localFav";
+import { Card, Grid } from "@nextui-org/react";
 
 const FavoritesPage = () => {
   const [favoritePokemons, setFavoritePokemons] = useState<number[]>([]);
@@ -12,7 +13,29 @@ const FavoritesPage = () => {
 
   return (
     <MainLayout title="Pokémons - Favoritos">
-      <NoFavorites></NoFavorites>
+      {favoritePokemons.length === 0 ? (
+        <NoFavorites></NoFavorites>
+      ) : (
+        <Grid.Container gap={2} direction={"row"} justify="center">
+          {favoritePokemons.map((id) => (
+            <Grid xs={6} sm={3} md={3} xl={1} key={id}>
+              <Card
+                isHoverable
+                isPressable
+                css={{
+                  padding: 10,
+                }}
+              >
+                <Card.Image
+                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
+                  width={"100%"}
+                  height={100}
+                ></Card.Image>
+              </Card>
+            </Grid>
+          ))}
+        </Grid.Container>
+      )}
     </MainLayout>
   );
 };
